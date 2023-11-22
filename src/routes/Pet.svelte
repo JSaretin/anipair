7db0ac925c5d76f4e66e64fb56eabbd22b18d1cf
JSaretin <jsaretin@gmail.com>
2023-11-22 23:19:59 +0100

    add booking

--- src/routes/Pet.svelte
+++ src/routes/Pet.svelte
@@ -3,11 +3,46 @@
 
 	export let pet: AniInfo;
 	let image = pet.images[0];
+
+	let book: boolean;
 </script>
 
 <div
-	class="rounded overflow-hidden shadow-md shadow-gray-300 flex flex-col h-[500px] 2xl:h-[350px]"
+	class="rounded relative overflow-hidden shadow-md shadow-gray-300 flex flex-col h-[500px] 2xl:h-[350px]"
 >
+	{#if book}
+		<!-- svelte-ignore a11y-no-static-element-interactions -->
+		<div
+			class="flex flex-col gap-4 overflow-y-scroll absolute inset-0 bg-black bg-opacity-80 backdrop-blur-sm p-4"
+			on:keyup
+			on:click|self={() => {
+				book = false;
+			}}
+		>
+			<label class="text-gray-200">
+				meeting date
+				<input class="p-1 mt-1 rounded text-gray-900" type="datetime-local" />
+			</label>
+
+			<label class="text-gray-200">
+				location
+				<textarea class="p-1 mt-1 rounded text-gray-900 resize-none" />
+			</label>
+
+			<label class="text-gray-200">
+				call number
+				<input type="text" class="p-1 mt-1 rounded text-gray-900" />
+			</label>
+
+			<label class="text-gray-200">
+				how many meeting
+				<input type="text" inputmode="numeric" class="p-1 mt-1 rounded text-gray-900" />
+			</label>
+
+			<button class="bg-green-500 p-2 rounded-md text-white font-bold">confirm booking</button>
+		</div>
+	{/if}
+
 	<div class="w-full group flex shrink-0 flex-1 overflow-hidden bg-gray-300">
 		<img
 			src="https://db.anipair.com/api/files/pets/{pet.id}/{image}"
@@ -39,7 +74,12 @@
 				class="text-xs flex justify-center align-middle place-items-center bg-gray-300 w-1/4 rounded text-gray-700 font-bold"
 				>$15,000</span
 			>
-			<button class="bg-green-500 p-2 lg:p-1 rounded-md text-white font-bold w-1/3">book</button>
+			<button
+				on:click={() => {
+					book = true;
+				}}
+				class="bg-green-500 p-2 lg:p-1 rounded-md text-white font-bold w-1/3">book</button
+			>
 		</div>
 	</div>
 </div>
