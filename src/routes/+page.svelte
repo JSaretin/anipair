02b01d6d80dd15cb8c6c7ab02044ec84c7d6d849
JSaretin <jsaretin@gmail.com>
2023-11-22 22:52:07 +0100

    rander small preview

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
-	import { env } from '$env/dynamic/public';
 	import type { AniInfo } from '$lib/structure';
 	import { writable, type Writable } from 'svelte/store';
-
-	// const imageUri = env.PUBLIC_DATABASE_URI;
+	import Pet from './Pet.svelte';
 
 	const aniInfos: Writable<AniInfo[]> = writable([]);
 
@@ -14,36 +12,8 @@
 <div class="w-full p-1 lg:p-6">
 	<h2 class="text-2xl text-gray-800 uppercase font-bold">Meeting</h2>
 	<div class="mt-6 text-sm grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-6 gap-6 w-full mx-auto">
-		{#each [...$aniInfos].reverse() as aniInfo (aniInfo.id)}
-			<div
-				class="rounded overflow-hidden shadow-md shadow-gray-300 flex flex-col h-[500px] 2xl:h-[350px]"
-			>
-				<div
-					class="w-full flex shrink-0 flex-1 overflow-x-scroll snap-proximity snap-x bg-gray-300"
-				>
-					{#each aniInfo.images as url, _}
-						<div class="shrink-0 w-full h-full snap-center">
-							<img
-								src="https://db.anipair.com/api/files/pets/{aniInfo.id}/{url}"
-								alt=""
-								class="w-full h-full object-cover"
-							/>
-						</div>
-					{/each}
-				</div>
-				<div class="p-2 flex flex-col gap-1">
-					<h2 class=" uppercase text-gray-700 font-medium">{aniInfo.title}</h2>
-					<div class="flex justify-between">
-						<span
-							class="text-xs flex justify-center align-middle place-items-center bg-gray-300 w-1/4 rounded text-gray-700 font-bold"
-							>$15,000</span
-						>
-						<button class="bg-green-500 p-2 lg:p-1 rounded-md text-white font-bold w-1/3"
-							>book</button
-						>
-					</div>
-				</div>
-			</div>
+		{#each [...$aniInfos].reverse() as pet (pet.id)}
+			<Pet {pet} />
 		{/each}
 	</div>
 </div>
---
+++ src/routes/Pet.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import type { AniInfo } from '$lib/structure';
+
+	export let pet: AniInfo;
+	let image = pet.images[0];
+</script>
+
+<div
+	class="rounded overflow-hidden shadow-md shadow-gray-300 flex flex-col h-[500px] 2xl:h-[350px]"
+>
+	<div class="w-full group flex shrink-0 flex-1 overflow-hidden bg-gray-300">
+		<img
+			src="https://db.anipair.com/api/files/pets/{pet.id}/{image}"
+			alt=""
+			class="w-full h-full object-cover group-hover:scale-105 transition-transform ease-in-out duration-300"
+		/>
+	</div>
+	<div class="flex p-2 gap-1 overflow-x-scroll">
+		{#each pet.images as url, _}
+			<button
+				class={'h-8 w-8 ' + (url == image ? 'border-2 border-blue-400' : '')}
+				on:click={() => {
+					if (url == image) return;
+					image = url;
+				}}
+			>
+				<img
+					src="https://db.anipair.com/api/files/pets/{pet.id}/{url}"
+					alt=""
+					class="w-full h-full object-cover"
+				/>
+			</button>
+		{/each}
+	</div>
+	<div class="p-2 flex flex-col gap-1">
+		<h2 class=" uppercase text-gray-700 font-medium">{pet.title}</h2>
+		<div class="flex justify-between">
+			<span
+				class="text-xs flex justify-center align-middle place-items-center bg-gray-300 w-1/4 rounded text-gray-700 font-bold"
+				>$15,000</span
+			>
+			<button class="bg-green-500 p-2 lg:p-1 rounded-md text-white font-bold w-1/3">book</button>
+		</div>
+	</div>
+</div>
